493dde0205f0e95ec61743b6f600e327c624ea79
tunachip <tunachiptv@gmail.com>
2025-10-24 21:13:26 -0400

    Added a Security Note for New Staff- they've been vibe coding too much

--- not_so_secret.py
+++ not_so_secret.py
@@ -1,1 +1,5 @@
 secret = "My Password i BIGCOW69_PIGLOVER_BUTNOTINAWEIRDWAY!"
+
+# Note to Staff
+# These passwords are very sensitive, and if they are leaked we could easily see legal trouble
+# Be VERY Careful with these~!
